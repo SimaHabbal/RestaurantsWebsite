d9294b04d0604cd6a5680299d9ac20ee3c498917
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-03-05 23:59:40 +0200

    fixing display

--- scripts/display.js
+++ scripts/display.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Retrieve items from local storage
-  const items = JSON.parse(localStorage.getItem("items")) || [];
+  //const items = JSON.parse(localStorage.getItem("items")) || [];
+  const items = JSON.parse(localStorage.getItem("restaurants")) || [];
 
   // Get the container to display items
   const container = document.getElementById("itemContainer");
@@ -11,44 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     card.className = "card";
 
     // Populate the card with item details
-    // card.innerHTML = `
-    //   <h2>${item.name}</h2>
-    //   <p>Price: $${item.price}</p>
-    //   <img src="${item.image}" alt="${item.name}" style="max-width: 100%;">
-    // `;
-
-
     card.innerHTML = `
-<section class="articles">
-  <article>
-    <div class="article-wrapper">
-      <figure>
-        <img src="${item.image}" alt="${item.name}" />
-      </figure>
-      <div class="article-body">
-        <h2>${item.name}</h2>
-     
-        <a href="#" class="read-more">
-          Read more <span class="sr-only">about this is some title</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </a>
-      </div>
-    </div>
-  </article>
-</section>;`
-
-
+      <h2>${item.name}</h2>
+      <p>Price: $${item.price}</p>
+      <img src="${item.imageUrl}" alt="${item.name}" style="max-width: 100%;">
+      <p>${item.description}</p>
+    `;
 
     // Append the card to the container
     container.appendChild(card);
