21b3fe220c491611cd86d6eb4f97532e84f6c638
unknown <hanadi.hosri@gmail.com>
2024-03-05 23:08:46 +0200

    adding login function

--- scripts/login.js
+++ scripts/login.js
@@ -2,3 +2,19 @@ let userCredentials = {};
 const username = document.getElementById("username").value;
 const password = document.getElementById("password").value;
 let message = document.getElementById("message")
+
+function login() {
+    if (username in userCredentials ) {
+        if (userCredentials[username] === password) {
+            window.location.href = "link to main"
+        } else {
+            message.innerHTML = "Incorrect Username/Password"
+        };   
+    } else {
+        message.innerHTML = "Username does not exist. Please sign up first."
+        showSignUp()
+        setTimeout(() => {
+            message.innerHTML = "";
+        }, 3000);
+    };
+};
