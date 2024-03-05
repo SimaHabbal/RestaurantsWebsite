c5b7b47db831dfbd3f6af529c8bc5d2116556c05
unknown <hanadi.hosri@gmail.com>
2024-03-05 23:07:28 +0200

    adding login.js

---
+++ scripts/login.js
@@ -0,0 +1,4 @@
+let userCredentials = {};
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+let message = document.getElementById("message")
